f2bbaf8378f6a8fc133b951a30af77d92f26a35b
Justin Zaun <justin.zaun@gmail.com>
2013-01-10 07:16:39 -1000

    Initial checkin on PHPixie

---
+++ README.md
@@ -0,0 +1,39 @@
+[PHPixie](http://phpixie.com)
+=======
+
+PHPixie is a lightweight PHP MVC framework designed for 
+rapid development, speed and simplicty. By avoiding as
+much boilerplate code as possible it allows for better 
+understanding of what the framework actually does and
+amazing performance.
+
+You can learn it in under 30 minutes using this tutorial:
+[Learn PHPixie in 30 minutes](http://phpixie.com/tutorials/learn-phpixie-in-30-minutes/).
+
+
+What to expect:
+---------------
+
+* Very Lightweight
+
+  Avoids as much boilerplate as possible to reduce your webserver load.
+
+* Super Fast
+
+  PHPixie was designed with speed in mind to provide lightning fast performance.
+
+* Well documented
+
+  Avoids as much boilerplate as possible to reduce your webserver load.
+
+* Rapid development
+
+  Avoids as much boilerplate as possible to reduce your webserver load.
+
+* Automagic
+
+  Avoids as much boilerplate as possible to reduce your webserver load.
+
+* Write your own code
+
+  Takes care of the foundation and lets you shape the rest any way you like.
---
+++ modules/database/classes/driver/pdo/db.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * PDO Database implementation.
+ * @package Database
+ */
+class DB_PDO_Driver extends DB{
+
+    /**
+     * Connection object
+     * @var PDO 
+     * @access public 
+	 * @link http://php.net/manual/en/class.pdo.php
+     */
+	public $conn;
+	
+	/**
+     * Type of the database, e.g. mysql, pgsql etc.
+     * @var string 
+     * @access public 
+     */
+	public $db_type;
+	
+    /**
+     * Initializes database connection
+     * 
+     * @param string $config Name of the connection to initialize
+     * @return void    
+     * @access public  
+     */
+	public function __construct($config) {
+		$this->conn = new PDO(
+			Config::get("database.{$config}.connection"),
+			Config::get("database.{$config}.user",''),
+			Config::get("database.{$config}.password",'')
+		);
+		$this->db_type=strtolower(str_replace('PDO_', '', $this->conn->getAttribute(PDO::ATTR_DRIVER_NAME)));
+	}
+
+    /**
+     * Builds a new Query implementation
+     * 
+     * @param string $type Query type. Available types: select,update,insert,delete,count
+     * @return Query_PDO_Driver  Returns a PDO implementation of a Query.
+     * @access public  
+	 * @see Query_Database
+     */
+	public function build_query($type) {
+		return new Query_PDO_Driver($this,$type);
+	}
+
+    /**
+     * Gets the id of the last inserted row.
+     * 
+     * @return mixed Row id
+     * @access public 
+     */
+	public function get_insert_id() {
+		if ($this->db_type == 'pgsql')
+			return $this->execute('SELECT lastval() as id')->current()->id;
+		return $this->conn->lastInsertId();
+	}
+
+    /**
+     * Executes a prepared statement query
+     * 
+     * @param string   $query  A prepared statement query
+     * @param array     $params Parameters for the query
+     * @return Result_PDO_Driver    PDO implementation of a database result
+     * @access public    
+     * @throws Exception If the query resulted in an error
+	 * @see Database_Result
+     */
+	public function execute($query, $params = array()) {
+		$cursor = $this->conn->prepare($query);
+		if(!$cursor->execute($params))
+			throw new Exception("Database error: ".implode(' ',$this->conn->errorInfo())." \n in query:\n{$query}");
+		return new Result_PDO_Driver($cursor);
+	}
+}
