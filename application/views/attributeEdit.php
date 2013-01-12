c3fee3b3aee616a45c34ca14ba4cf401f57ecde3
Justin Zaun <justin.zaun@gmail.com>
2013-01-12 11:38:36 -1000

    dummy text on the edit attribute view

---
+++ application/views/attributeEdit.php
@@ -0,0 +1,22 @@
+<div class='pageImage'>
+    <img src='<?php echo $pageImage; ?>' />
+    <label>Hello World</label>
+</div>
+<h1>Section Header</h1>
+<div class="row">
+    <label>Test</label>
+    <data>Value</data>
+</div>
+<div class="row">
+    <label>Succeeded by</label>
+    <data>Kwame Raoul</data>
+</div>
+<h1>Section Header</h1>
+<div class="row">
+    <label>Test</label>
+    <data>Value</data>
+</div>
+<div class="row">
+    <label>Succeeded by</label>
+    <data>Kwame Raoul</data>
+</div>
